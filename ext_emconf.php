e5dbcf474c88fe91ddc2fef7aa0f126152dc5d92
Johannes C. Schulz <johannes.schulz@kvt.de>
2019-09-03 13:42:19 +0200

    CacheDatabaseBackend throws error "Cannot call abstract method TYPO3\CMS\Core\Cache\Backend\BackendInterface::set()" . Don't know how to fix that.

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'_md5_values_when_last_written' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.5.0-9.5.99'
+			'typo3' => '8.7.0-9.5.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
