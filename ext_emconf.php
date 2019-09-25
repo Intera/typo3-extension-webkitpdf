9c0e14ee2a7db4485b375b15818fd2649c17c4e5
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-09-25 22:00:39 +0200

    [CLEANUP] Code cleanup in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,23 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Webkit PDFs',
-	'description' => 'Generate PDF files using WebKit rendering engine.',
-	'category' => 'plugin',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => 'typo3temp/tx_webkitpdf/',
-	'clearCacheOnLoad' => 0,
-	'author' => 'Dev-Team Typoheads',
-	'author_email' => 'dev@typoheads.at',
-	'author_company' => 'Typoheads GmbH',
-	'version' => '2.0.0-dev',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.3-8.7.99'
-		),
-		'conflicts' => array(),
-		'suggests' => array(),
-	),
-);
+/** @noinspection PhpMissingStrictTypesDeclarationInspection */
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Webkit PDFs',
+    'description' => 'Generate PDF files using WebKit rendering engine.',
+    'category' => 'plugin',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => 'typo3temp/tx_webkitpdf/',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Dev-Team Typoheads',
+    'author_email' => 'dev@typoheads.at',
+    'author_company' => 'Typoheads GmbH',
+    'version' => '2.0.0-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.3-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
